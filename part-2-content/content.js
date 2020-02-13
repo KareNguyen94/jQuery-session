2e754dab01342bc30d22a5d53e2f1539fa4aa03d
Karen Nguyen <knguyen4184@yahoo.com>
2020-02-13 10:19:52 -0700

    Practice 3

--- part-2-content/content.js
+++ part-2-content/content.js
@@ -1,4 +1,5 @@
-const $ = require('jquery');
+// const $  require('jquery');
+
 let btn = document.querySelector('button');
 btn.addEventListener('click', function(e){
   e.preventDefault();
@@ -12,3 +13,10 @@ clearBtn.addEventListener('click', function(){
 })
 
 console.log($('p').text())
+
+$('button').text('Add New Buddy')
+$('label[for=confirm]').text('Yes!!!')
+$('label[for=deny]').text('No!!!')
+$('input[value=Jeff]').val('karen')
+$('input[type=email]').val('unicorn@yahoo.com')
+$('h1, h2').text('contact form')
